b382ddd30bda1b9743e43c6f41876d55eea56a89
Christin chacko <christinpvk@gmail.com>
2019-10-03 23:55:11 +0530

    ScheduleList-API Call-Axios

--- src/layout/schedule/ScheduleList.js
+++ src/layout/schedule/ScheduleList.js
@@ -2,92 +2,96 @@ import React, { Component } from "react";
 import { Row, Col } from "reactstrap";
 import ScheduleListItem from "./ScheduleListItem";
 import Quicklinks from "../quicklink/Quicklinks";
+import axios from 'axios';
 
 class ScheduleList extends Component {
   state = {
-    schedulelistnew: [],
-    schedulelist: [
-      {
-        id: 1,
-        category: "premier league",
-        firstteam: "arsenal",
-        secondteam: "chelsea",
-        firstteamlogo: "logo-arsenal.png",
-        secondteamlogo: "logo-chelsea.png",
-        stadium: "emirates stadium",
-        time: "7:00",
-        timezone: "pm",
-        date: "8",
-        day: "wednesday",
-        month: "sep",
-        year: "2019"
-      },
-      {
-        id: 2,
-        category: "friendly",
-        firstteam: "barcelona",
-        secondteam: "chelsea",
-        firstteamlogo: "logo-barcelona.png",
-        secondteamlogo: "logo-chelsea.png",
-        stadium: "camp nou",
-        time: "8:00",
-        timezone: "pm",
-        date: "11",
-        day: "saturday",
-        month: "sep",
-        year: "2019"
-      },
-      {
-        id: 3,
-        category: "premier league",
-        firstteam: "chelsea",
-        secondteam: "manchester city",
-        firstteamlogo: "logo-chelsea.png",
-        secondteamlogo: "logo-m-city.png",
-        stadium: "stamford bridge",
-        time: "9:30",
-        timezone: "pm",
-        date: "15",
-        day: "wednesday",
-        month: "sep",
-        year: "2019"
-      },
-      {
-        id: 4,
-        category: "premier league",
-        firstteam: "liverpool",
-        secondteam: "chelsea",
-        firstteamlogo: "logo-liverpool.png",
-        secondteamlogo: "logo-chelsea.png",
-        stadium: "anfield",
-        time: "7:00",
-        timezone: "am",
-        date: "17",
-        day: "friday",
-        month: "sep",
-        year: "2019"
-      },
-      {
-        id: 5,
-        category: "friendly",
-        firstteam: "chelsea",
-        secondteam: "real madrid",
-        firstteamlogo: "logo-chelsea.png",
-        secondteamlogo: "logo-real-madrid.png",
-        stadium: "stamford bridge",
-        time: "5:00",
-        timezone: "pm",
-        date: "22",
-        day: "wednesday",
-        month: "sep",
-        year: "2019"
-      }
-    ]
+    schedulelistnew: []
+    // ,schedulelist: []
+      // {
+      //   id: 1,
+      //   category: "premier league",
+      //   firstteam: "arsenal",
+      //   secondteam: "chelsea",
+      //   firstteamlogo: "logo-arsenal.png",
+      //   secondteamlogo: "logo-chelsea.png",
+      //   stadium: "emirates stadium",
+      //   time: "7:00",
+      //   timezone: "pm",
+      //   date: "8",
+      //   day: "wednesday",
+      //   month: "sep",
+      //   year: "2019"
+      // },
+      // {
+      //   id: 2,
+      //   category: "friendly",
+      //   firstteam: "barcelona",
+      //   secondteam: "chelsea",
+      //   firstteamlogo: "logo-barcelona.png",
+      //   secondteamlogo: "logo-chelsea.png",
+      //   stadium: "camp nou",
+      //   time: "8:00",
+      //   timezone: "pm",
+      //   date: "11",
+      //   day: "saturday",
+      //   month: "sep",
+      //   year: "2019"
+      // },
+      // {
+      //   id: 3,
+      //   category: "premier league",
+      //   firstteam: "chelsea",
+      //   secondteam: "manchester city",
+      //   firstteamlogo: "logo-chelsea.png",
+      //   secondteamlogo: "logo-m-city.png",
+      //   stadium: "stamford bridge",
+      //   time: "9:30",
+      //   timezone: "pm",
+      //   date: "15",
+      //   day: "wednesday",
+      //   month: "sep",
+      //   year: "2019"
+      // },
+      // {
+      //   id: 4,
+      //   category: "premier league",
+      //   firstteam: "liverpool",
+      //   secondteam: "chelsea",
+      //   firstteamlogo: "logo-liverpool.png",
+      //   secondteamlogo: "logo-chelsea.png",
+      //   stadium: "anfield",
+      //   time: "7:00",
+      //   timezone: "am",
+      //   date: "17",
+      //   day: "friday",
+      //   month: "sep",
+      //   year: "2019"
+      // },
+      // {
+      //   id: 5,
+      //   category: "friendly",
+      //   firstteam: "chelsea",
+      //   secondteam: "real madrid",
+      //   firstteamlogo: "logo-chelsea.png",
+      //   secondteamlogo: "logo-real-madrid.png",
+      //   stadium: "stamford bridge",
+      //   time: "5:00",
+      //   timezone: "pm",
+      //   date: "22",
+      //   day: "wednesday",
+      //   month: "sep",
+      //   year: "2019"
+      // }
+    //]
   };
 
   componentDidMount() {
-    this.setState({
-      schedulelistnew: this.state.schedulelist
+    axios.get(`https://apiv2.apifootball.com/?action=get_H2H&firstTeam=Chelsea&secondTeam=Arsenal&APIkey=7d0c5d199386447bea7e7de45908eeb494ec18c5127ef7b6fa05b8a3f4f5155f`)
+    .then(res => {
+       const schedulelistnew = res.data.firstTeam_VS_secondTeam;
+       console.log(res.data);
+      this.setState({ schedulelistnew });
     });
   }
 
@@ -97,7 +101,7 @@ class ScheduleList extends Component {
         <Row>
           <Col lg="9" md="8">
             {this.state.schedulelistnew.map(item => (
-              <div className="mb-3 pb-2" key={item.id}>
+              <div className="mb-3 pb-2" key={item.match_id}>
                 <ScheduleListItem item={item} />
               </div>
             ))}
@@ -108,6 +112,8 @@ class ScheduleList extends Component {
         </Row>
       </div>
     );
+
+    
   }
 }
 
--- src/layout/schedule/ScheduleListItem.js
+++ src/layout/schedule/ScheduleListItem.js
@@ -15,13 +15,13 @@ class ScheduleListItem extends Component {
       <div>
         <Card className="b-radius-0 bg-header-outer">
           <CardHeader className="bg-header-block d-flex justify-content-between align-items-center p-0">
-            <div className="date-box">{this.props.item.date}</div>
+            <div className="date-box">{this.props.item.match_date}</div>
             <div className="day-box d-flex align-items-center justify-content-between w-100">
               <div className="overlay"></div>
               <p className="text-white mb-0 zi-99">
-                <span className="day">{this.props.item.day}</span>
-                <span className="month ml-1">{this.props.item.month}</span>
-                <span className="year ml-1">{this.props.item.year}</span>
+                <span className="day">{this.props.item.match_date}</span>
+                {/* <span className="month ml-1">{this.props.item.month}</span>
+                <span className="year ml-1">{this.props.item.year}</span> */}
               </p>
             </div>
           </CardHeader>
@@ -31,37 +31,37 @@ class ScheduleListItem extends Component {
                 <div className="logo-block">
                   <div className="logo">
                     <img
-                      src={"../images/" + this.props.item.firstteamlogo}
+                      src="../images/logo-real-madrid.png" alt="logo-real-madrid.png"
                     ></img>
                   </div>
                 </div>
                 <div className="team-block">
                   <div className="teams text-center">
                     <div className="team">
-                      <h6>{this.props.item.firstteam}</h6>
+                      <h6>{this.props.item.match_hometeam_name}</h6>
                     </div>
                     <div className="versus">vs</div>
                     <div className="team">
-                      <h6>{this.props.item.secondteam}</h6>
+                      <h6>{this.props.item.match_awayteam_name}</h6>
                     </div>
                     <Badge
                       className="ml-auto match-category league zi-99 mt-3"
                       color={
-                        this.props.item.category === "premier league"
+                        this.props.item.league_name === "Europa League"
                           ? "league"
-                          : this.props.item.category === "super-league"
+                          : this.props.item.league_name === "Premier League"
                           ? "super-league"
                           : "friendly"
                       }
                     >
-                      {this.props.item.category}
+                      {this.props.item.league_name}
                     </Badge>
                   </div>
                 </div>
                 <div className="logo-block">
                   <div className="logo">
                     <img
-                      src={"../images/" + this.props.item.secondteamlogo}
+                      src="../images/logo-chelsea.png" alt="logo-chelsea.png"
                     ></img>
                   </div>
                 </div>
@@ -71,9 +71,9 @@ class ScheduleListItem extends Component {
           <CardFooter className="b-radius-0 bg-white">
             <div className="venue-block">
               <p>
-                <span className="stadium">{this.props.item.stadium}</span>
+                <span className="stadium">{this.props.item.match_hometeam_name}</span>
                 <span className="ml-2 mr-2">|</span>
-                <span className="time">{this.props.item.time}</span>
+                <span className="time">{this.props.item.match_time}</span>
                 <span className="timezone ml-1">
                   {this.props.item.timezone}
                 </span>
